1a28f5e9b158dfe5e7223c60f336954148a49eb4
rubrym <empireofgame@me.com>
2021-12-04 14:47:40 +0100

    ejercicio 1 repaso

--- ejercicios-react/src/ejercicios/ejercicio1.js
+++ ejercicios-react/src/ejercicios/ejercicio1.js
@@ -5,13 +5,19 @@ import "./ejercicio1.css";
 
 //Muestras todas las personas usando el componente card
 
-
 const Ejercicio1 = () => {
   return (
     <div className="espace">
       <h1> Personas </h1>
       <div className="card">
-        {/*Map de las cards con la info de cada persona*/}
+        {personas.map((persona) => (
+          <Card
+            src={persona.src}
+            name={persona.name}
+            surname={persona.surname}
+            age={persona.age}
+          />
+        ))}
       </div>
     </div>
   );
