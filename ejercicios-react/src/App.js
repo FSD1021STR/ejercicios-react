1b7e3bd5c6e141b8a8e7312fcd17dda7dfba1248
Sergio Radigales <sergioradigales@gmail.com>
2021-12-10 10:19:43 +0100

    Como usar el componente Card, con los datos de constantes dentro del componente Ejercicio1 SIN ARRAY

--- ejercicios-react/src/App.js
+++ ejercicios-react/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Ejercicio1 from './ejercicios/ejercicio1';
 
 function App() {
   return (
     <div className="App">
-    
+    <Ejercicio1/>
     </div>
   );
 }
--- ejercicios-react/src/ejercicios/ejercicio1.js
+++ ejercicios-react/src/ejercicios/ejercicio1.js
@@ -11,6 +11,29 @@ const Ejercicio1 = () => {
     <div className="espace">
       <h1> Personas </h1>
       <div className="card">
+        <Card src={personas[0].src} name={personas[0].name} 
+        surname={personas[0].surname} age={personas[0].age}/>
+
+        <Card src={personas[1].src} name={personas[1].name} 
+        surname={personas[1].surname} age={personas[1].age}/>
+
+        <Card src={personas[2].src} name={personas[2].name} 
+        surname={personas[2].surname} age={personas[2].age}/>
+
+        <Card src={personas[3].src} name={personas[3].name} 
+        surname={personas[3].surname} age={personas[3].age}/>
+
+        <Card src={personas[4].src} name={personas[4].name} 
+        surname={personas[4].surname} age={personas[4].age}/>
+
+        <Card src={personas[5].src} name={personas[5].name} 
+        surname={personas[5].surname} age={personas[5].age}/>
+
+        <Card src={personas[6].src} name={personas[6].name} 
+        surname={personas[6].surname} age={personas[6].age}/>
+
+        <Card src={personas[7].src} name={personas[7].name} 
+        surname={personas[7].surname} age={personas[7].age}/>
         {/*Map de las cards con la info de cada persona*/}
       </div>
     </div>
